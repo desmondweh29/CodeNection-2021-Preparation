8d49e6d43de9e414bb16b411f2a4a05efd199802
Desmond Wong <62997554+desmondweh29@users.noreply.github.com>
2021-12-08 19:24:30 +0800

    Add files via upload

---
+++ src/Another_Sorting_Problem.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Question: https://codeforces.com/problemset/problem/1575/A
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n, m; //n - number of books, m - length of book(s)'s name
+    cin >> n >> m;
+
+    vector <pair<string, int>> books(n);
+    string book;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> book;        
+        books[i] = {book, i + 1};
+    }
+
+    sort(books.begin(), books.end());
+
+    bool even = true;
+    bool sorted = false;
+
+    for (int j = 1; j < m; j++)
+    {
+        long long start = 0;
+        long long end;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+           if (books[i].first.substr(0,j) != books[i + 1].first.substr(0,j))
+           {
+              end = n - i - 1;
+              if (even)
+              {
+                 sort(books.rbegin() + end, books.rend() - start);
+              }
+              else
+              {
+                 sort(books.begin() + start, books.end() - end);
+              }
+              start = i + 1;
+
+              if (i == n - 2)
+              {
+                 sorted = true;
+              }
+           }
+        }
+
+         if (!sorted)
+         {
+            if (even)
+            {
+               sort(books.rbegin(), books.rend() - start);
+            }
+            else
+            {
+               sort(books.begin() + start, books.end());
+            }
+         }
+         even = !even;
+    }
+
+    for (auto i : books)
+    {
+        cout << i.second << " ";
+    }
+    return 0;
+}
